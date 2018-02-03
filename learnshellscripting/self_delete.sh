29719ff961921948a887e02f9ddbf1f00e301e4e
sarathvellat <sarath_v@icloud.com>
2018-02-03 13:34:50 -0800

    Adding shell script example

---
+++ learnshellscripting/self_delete.sh
@@ -0,0 +1,9 @@
+#!/bin/rm
+
+# Self deleting Script
+
+WHTEVER=85
+
+echo "This is not going to ouput anything, instead the file will disappar"
+
+exit $WHAREVER			# but still the script will exit with status 0, not 85
